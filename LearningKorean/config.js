8c036e4ae53c38ca5cadabaa71f97d4480a24a6b
nanowind0315 <nanowind@gmail.com>
2025-09-11 22:08:48 +0900

    Update config.js

--- LearningKorean/config.js
+++ LearningKorean/config.js
@@ -2,8 +2,8 @@ const CONFIG = {
   // Google Sheets CSV URL
   GOOGLE_SHEETS_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4r0QH4aGs4kERDCLWYb4uY-XIa891Uc6BzGCKrdwLv9-gcDQ6-ju0fbsgJpwezOWSH30dGqg-Hid8/pub?gid=1486372661&single=true&output=csv',
 
-  // GitHub Pages 에셋 기본 URL (배포 후 필요시 확인)
-  ASSETS_BASE_URL: 'https://nanowindlab.github.io/LearningKorean',
+  // 현재 위치를 기준으로 에셋 경로를 찾도록 설정
+  ASSETS_BASE_URL: '.',
 
   // 개발 모드 설정
   USE_LOCAL: false,
